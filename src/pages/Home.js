8d7853a58f2582027d72f0e00d9af46488efa3ba
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2025-03-06 20:19:19 +0530

    Minor Changes and About sections done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from '../components/Navbar/NavBar';
 import Hero from '../components/HeroSection/HeroSection';
+import About from '../components/About/About';
 
 const Home = () => {
   return (
     <>
       <NavBar />
       <Hero />
+      <About />
     </>
   );
 };
