a0778e0eb41a93127ffc75f67cb952e30a72be1c
NirajSrivastava18 <nirajsrivastava2001@gmail.com>
2025-03-07 19:02:02 +0530

    Some CSS changes and Responsive design completed

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Menu, X } from 'lucide-react';
 import './NavBar.css';
 import brand from '../../assets/brand.svg';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="navContainer">
+    <nav className="navContainer">
       <div className="navlogo">
         <img src={brand} alt="brand" />
       </div>
-      <div className="navLinks">
+      <div className={`navLinks ${isOpen ? 'open' : ''}`}>
         <button>Home</button>
         <button>About</button>
         <button>Projects</button>
@@ -17,7 +20,10 @@ const NavBar = () => {
       <div className="navButtons">
         <button>HIRE ME</button>
       </div>
-    </div>
+      <div className="menuIcon" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <X size={30} /> : <Menu size={30} />}
+      </div>
+    </nav>
   );
 };
 
